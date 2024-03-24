6017bb7282833ac9af9eb17a4379120f1d3190d6
cedrotech1 <cedrotech1@gmail.com>
2024-03-24 08:54:22 +0200

    corrected seeds

--- src/Database/seeders/20231102134332-demo-user.js
+++ src/Database/seeders/20231102134332-demo-user.js
@@ -1,110 +1,27 @@
 'use strict';
+const bcrypt = require("bcrypt");
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    await queryInterface.bulkInsert('Users', [
-      {
-        id:1,
-      firstName: 'Alexis',
-      lastName: 'HAKIZIMANA',
-      profile: 'Alexis.jgp',
-      email: 'test@alexis.com',
-      password: '123',
+  async up(queryInterface, Sequelize) {
+    const saltRounds = 10; // Number of salt rounds for bcrypt
+
+    const hashedPasswordAdmin = await bcrypt.hash("umwiza", saltRounds);
+
+    await queryInterface.bulkInsert('Users', [{
+      firstName: 'UMWIZA',
+      lastName: 'Maureen',
+      profile: 'https://res.cloudinary.com/da12yf0am/image/upload/v1711263166/pmeqjiziugbrath7vkps.jpg',
+      email: 'maureekalala@gmail.com', 
+      role: 'admin',
+      password: hashedPasswordAdmin,
       createdAt: new Date(),
       updatedAt: new Date()
-    },
-    {
-      id: 2,
-      firstName: 'Steven',
-      lastName: 'MUKAMA',
-      profile: 'mukama.jgp',
-      email: 'test@mukama.com',
-      password: '345',
-      createdAt: new Date(),
-      updatedAt: new Date()
-    },
-    {
-      id: 3,
-      firstName: 'Viateur',
-      lastName: 'MANIEAGENA',
-      profile: 'viateur.jgp',
-      email: 'test@viateur.com',
-      password: '789',
-      createdAt: new Date(),
-      updatedAt: new Date()
-    },
-  ], 
-  {}
-  );
-  await queryInterface.bulkInsert('Posts', [
-    {
-      id:1,
-    postTitle: 'Coffee',
-    postImage: 'coffee.jpg',
-    postContent: 'Nyamara nibyo',
-    postedBy: 2,
-    createdAt: new Date(),
-    updatedAt: new Date()
-  },
-  {
-    id:2,
-    postTitle: 'Codding',
-    postImage: 'code.jpg',
-    postContent: 'Codding is amazing',
-    postedBy: 1,
-    createdAt: new Date(),
-    updatedAt: new Date()
-  },
-  
-], 
-{}
-);
-await queryInterface.bulkInsert('Comments', [
-  {
-    id:1,
-  commentBody: 'Great training',
-  postId: 2,
-  CommentedBy: 3,
-  createdAt: new Date(),
-  updatedAt: new Date()
-},
-{
-  id:2,
-  commentBody: 'Coffee is mine',
-  postId: 1,
-  CommentedBy: 3,
-  createdAt: new Date(),
-  updatedAt: new Date()
-},
+    }], {});
 
-], 
-{}
-);
-await queryInterface.bulkInsert('Replies', [
-  {
-    id:1,
-  replyMessage: 'Thank you so much',
-  commentId: 1,
-  repliedBy: 2,
-  createdAt: new Date(),
-  updatedAt: new Date()
-},
-{
-  id:2,
-  replyMessage: 'Add more efforts',
-  commentId: 2,
-  repliedBy: 3,
-  createdAt: new Date(),
-  updatedAt: new Date()
-},
-
-], 
-{}
-);
   },
 
-  async down (queryInterface, Sequelize) {
+  async down(queryInterface, Sequelize) {
     /**
      * Add commands to revert seed here.
      *
